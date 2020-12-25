0d8db567db160cc2831b8f6545d3c8531dd66e7f
yonggoonoh <nohyg90@gmail.com>
2020-12-25 13:07:09 +0900

    Show dice

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// 6 colors: white, orange, yellow, blue, green, red
-const COLORS = ["W", "O", "Y", "B", "G", "R"];
+const COLORS = ["white", "orange", "yellow", "blue", "green", "red"];
 
 function getRandomColor() {
     return COLORS[Math.random() * COLORS.length | 0]
@@ -25,3 +24,19 @@ const myColors = COLORS.reduce((acc, c) => {
 
     return acc;
 }, []);
+
+const diceDiv = document.getElementById("dice");
+
+rollDice().forEach(row => {
+    const parent = document.createElement("div");
+
+    row.forEach(col => {
+        const div = document.createElement("div");
+
+        div.style.backgroundColor = col;
+
+        parent.appendChild(div);
+    });
+
+    diceDiv.appendChild(parent);
+});
