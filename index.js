0e69045f0f222db02b6e16bec8f87d9d5d52e4be
yonggoonoh <nohyg90@gmail.com>
2020-12-25 12:51:23 +0900

    Implement roll dice

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+// 6 colors: white, orange, yellow, blue, green, red
+const COLORS = ["W", "O", "Y", "B", "G", "R"];
+
+
+function getRandomColor() {
+    return COLORS[Math.random() * COLORS.length | 0]
+}
+
+function rollDice() {
+    const result = [[], [], []];
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            result[i][j] = getRandomColor();
+        }
+    }
+
+    return result;
+}
+
+console.log(rollDice());
